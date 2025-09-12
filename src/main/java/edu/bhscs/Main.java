73bddc7c3c177b4388a5a457b5c576d822b4f96c
AliZa1di <2010380@apps.nsd.org>
2025-09-12 16:48:09 +0000

    add print lines

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,24 @@
-// Student name
-// class period
-// project name
-// date
+// Ali Zaidi
+// P2
+// Zero-Hero
+// 9/12/25
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr Reiber says
+ * INPUT: We dont know about input yet
+ * OUTPUT: Some cools words in the command line thing
+ * EDGE CASE: They are all edge cases at the moment
  */
 
 package edu.bhscs;
+
+
+class Main{
+  public static void main( String[] args ){
+    System.out.println("-------------------");
+    System.out.println("!!!! PROGRAM START !!!!");
+    System.out.println("Hello");
+    System.out.println("!!!! PROGRAM END !!!!");
+    System.out.println("-------------------");
+  }
+}
