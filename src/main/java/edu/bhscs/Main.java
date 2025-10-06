c0718b6ea01ecb01f46ec681f49229a4021b8c27
AliZa1di <2010380@apps.nsd.org>
2025-10-06 16:23:04 +0000

    I think I've done everything and Im done. I think this is good for someone liek me that isnt too good with code.

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,15 @@
 package edu.bhscs;
 
 import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
-    Scanner s = new Scanner( System.in );
+    Scanner s = new Scanner(System.in);
     System.out.println("Welcome to Bob's Baking Bakery!");
-    System.out.println("What cake would you like to purchase? We have chocolate, vanilla, and strawberry.");
+    System.out.println(
+        "What cake would you like to purchase? We have chocolate, vanilla, and strawberry.");
     String cake = s.next();
     System.out.println("We are making your: " + cake + " cake");
     System.out.println("It'll be fresh and ready, just a moment!");
@@ -21,19 +23,14 @@ public class Main {
       System.out.println("Here's your strawberry cake!");
     } else {
       System.out.println("Sorry, we out of stock for that right now. Come back tmrw maybe.");
-
-    }
-
     }
   }
+}
 
-
-
-
-//old code
-//StorePersonDude clerk = new StorePersonDude("Gus");
+// old code
+// StorePersonDude clerk = new StorePersonDude("Gus");
   //  Store store = new Store("Bob's Baking Bakery", clerk);
-    //Person person = new Person("Bob");
-    //System.out.println("Welcome to " + store.getName() + "!!!!!!");
-    //person.buyAndEatCake(store);
-  //}
+    // Person person = new Person("Bob");
+    // System.out.println("Welcome to " + store.getName() + "!!!!!!");
+    // person.buyAndEatCake(store);
+  // }
