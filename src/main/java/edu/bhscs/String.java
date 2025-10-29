e7bd7416bb00f57bdfa4f0f66edc5eec3ff031de
AliZa1di <2010380@apps.nsd.org>
2025-10-29 15:57:39 +0000

    todays lesson in my code

---
+++ src/main/java/edu/bhscs/String.java
@@ -0,0 +1,19 @@
+package edu.bhscs;
+
+/*public class String {
+
+  char[] theString;
+
+public String(char[] theString){
+this.theString = theString;
+}
+
+
+
+public String subString(int start, int end){
+  String demo = "the wrong string duder";
+  return demo;
+
+}
+}
+*/
