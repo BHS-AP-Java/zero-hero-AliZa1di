ef6b0e12cbbe5d09f4db9ced7346a2b4e3c41378
AliZa1di <2010380@apps.nsd.org>
2025-11-05 09:46:35 -0800

    NakedAndAfraidProcessFLIPPITYTHEGODS

---
+++ src/main/java/edu/bhscs/App.java
@@ -0,0 +1,18 @@
+package edu.bhscs;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    date_11_5_25();
+
+
+  }
+
+  public static void date_11_5_25(){
+    System.out.println("Flippity the gods");
+
+    new Table();
+  }
+}
---
+++ src/main/java/edu/bhscs/Table.java
@@ -0,0 +1,16 @@
+package edu.bhscs;
+
+public class Table {
+
+  //Properties
+
+
+
+  //Constructor
+
+
+
+  
+  //Methods
+
+}
